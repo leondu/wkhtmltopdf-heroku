82d8cd4b82029bc648c7eeeadd311f477cef44d5
Leon Du <leondu@gmail.com>
2011-12-05 01:30:17 +0800

    config with WickedPdf

--- lib/wkhtmltopdf-heroku.rb
+++ lib/wkhtmltopdf-heroku.rb
@@ -1,10 +1,1 @@
-# config/initializers/pdfkit.rb
-require 'pdfkit'
-PDFKit.configure do |config|
-  config.wkhtmltopdf = File.expand_path "../../bin/wkhtmltopdf-linux-amd64", __FILE__
-  # config.default_options = {
-  #   :page_size => 'Legal',
-  #   :print_media_type => true
-  # }
-  # config.root_url = "http://localhost" # Use only if your external hostname is unavailable on the server.
-end
+WickedPdf.config[:exe_path] = File.expand_path "../../bin/wkhtmltopdf-linux-amd64", __FILE__
